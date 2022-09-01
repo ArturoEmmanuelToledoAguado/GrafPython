d57eac28d9f6598fed281b30b3720e515c8c2928
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-01 18:17:13 -0500

    Cambios Esteticos

--- 1.py
+++ 1.py
@@ -3,6 +3,8 @@ from tkinter import *
 root=Tk()
 contador = 0
 v = StringVar()
+root.title("Contador")
+
 etiqueta = Label(root, textvariable=v, font="Helvetica 48")
 etiqueta.pack()
 
@@ -11,6 +13,7 @@ def sumar():
     contador = contador + 1    
     v.set(str(contador))
 
-button1 = Button(root, text='sumar', width=25, command=sumar)
+button1 = Button(root, text='+', width=25, command=sumar)
+
 button1.pack()
 root.mainloop() 
